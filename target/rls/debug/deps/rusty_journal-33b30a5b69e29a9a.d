ea7e6fe3907cd5b1642157cfb5e8b5f40c0476b7
Eri Mendz <erimendz@gmail.com>
2021-06-09 13:52:34 +0300

    all all commits since last edit

--- target/rls/debug/deps/rusty_journal-33b30a5b69e29a9a.d
+++ target/rls/debug/deps/rusty_journal-33b30a5b69e29a9a.d
@@ -1,6 +1,6 @@
-/home/eribertt/projects/rusty-journal/target/rls/debug/deps/rusty_journal-33b30a5b69e29a9a.rmeta: src/main.rs src/cli.rs
+/home/eribertt/coding/rust/rusty-journal/target/rls/debug/deps/rusty_journal-33b30a5b69e29a9a.rmeta: src/main.rs src/cli.rs
 
-/home/eribertt/projects/rusty-journal/target/rls/debug/deps/rusty_journal-33b30a5b69e29a9a.d: src/main.rs src/cli.rs
+/home/eribertt/coding/rust/rusty-journal/target/rls/debug/deps/rusty_journal-33b30a5b69e29a9a.d: src/main.rs src/cli.rs
 
 src/main.rs:
 src/cli.rs:
