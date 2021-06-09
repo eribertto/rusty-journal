91b2a72c0c24ae8365663b0a839c2f10156f52bc
Eri Mendz <erimendz@gmail.com>
2021-06-09 14:39:23 +0300

    add dot gitignore file and populate this

--- target/debug/rusty-journal.d
+++ target/debug/rusty-journal.d
@@ -1,1 +1,1 @@
-/home/eribertt/projects/rusty-journal/target/debug/rusty-journal: /home/eribertt/projects/rusty-journal/src/cli.rs /home/eribertt/projects/rusty-journal/src/main.rs
+/home/eribertt/coding/rust/rusty-journal/target/debug/rusty-journal: /home/eribertt/coding/rust/rusty-journal/src/cli.rs /home/eribertt/coding/rust/rusty-journal/src/main.rs
