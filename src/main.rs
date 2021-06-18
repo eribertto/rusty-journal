2f8e661c5f25f694de22c22eeb1dccf514f2144c
Eriberto Mendez <eribert.mendez@gmail.com>
2021-06-18 17:11:37 +0300

    add anyhow codes to give friendly error warning

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 // to complete this exercise
 
 // import cli and tasks modules
-mod tasks;
-mod cli;
+use anyhow::anyhow;
+use std::path::PathBuf;
 use structopt::StructOpt;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
-// add std path library
-use std::path::PathBuf;
+mod tasks;
+mod cli;
 
 // new function to use the home crate in Cargo.toml
 fn find_default_journal_file() -> Option<PathBuf> {
@@ -18,8 +18,8 @@ fn find_default_journal_file() -> Option<PathBuf> {
 	})
 }
 
-
-fn main() {
+// fn main to return the type anyhow::Result<()>
+fn main() -> anyhow::Result<()> {
     // get the command-line args
 	let CommandLineArgs {
 		action,
@@ -29,16 +29,15 @@ fn main() {
 	// new version
 	let journal_file = journal_file
 	.or_else(find_default_journal_file)
-	.expect("Failed to find journal file.");
+	.ok_or(anyhow!("Failed to find journal file."))?;
 
-	// unpack the journal
-	//let journal_file = journal_file.expect("Failed to find journal file");
-	// perform the action with match
 
+	// new match block, note the symbol ? after the closing curly
+	// to remove the expect calls from our code
 	match action {
 		Add { text } => tasks::add_task(journal_file, Task::new(text)),
 		List => tasks::list_tasks(journal_file),
 		Done { position } => tasks::complete_task(journal_file, position),
-	}
-	.expect("Failed to perform action")
+	}?;
+	Ok(())
 }
