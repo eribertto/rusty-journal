a1bd9eb9a52b3c7c62ce7df883dede2cd86e7ef8
Eriberto Mendez <eribert.mendez@gmail.com>
2021-06-18 13:03:46 +0300

    modify main.rs to call the other function files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 // june 18/21 start in new branch regolith
 // to complete this exercise
 
+// import cli and tasks modules
+mod tasks;
 mod cli;
 use structopt::StructOpt;
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
 
-//fn main() {
-//    cli::CommandLineArgs::from_args();
-//}
 fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+    // get the command-line args
+	let CommandLineArgs {
+		action,
+		journal_file,
+	} = CommandLineArgs::from_args();
+
+	// unpack the journal
+	let journal_file = journal_file.expect("Failed to find journal file");
+	// perform the action with match
+	match action {
+		Add { text } => tasks::add_task(journal_file, Task::new(text)),
+		List => tasks::list_tasks(journal_file),
+		Done { position } => tasks::complete_task(journal_file, position),
+	}
+	.expect("Failed to perform action")
 }
