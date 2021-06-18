8eed5581ed7c6285ede4d9fa5293050c646ae7f9
Eriberto Mendez <eribert.mendez@gmail.com>
2021-06-18 12:28:57 +0300

    add list_tasks function file

---
+++ src/list_task.rs
@@ -0,0 +1,21 @@
+// define list_tasks function
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    // Open the file.
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    // Parse the file and collect the tasks.
+    let tasks = collect_tasks(&file)?;
+
+    // Enumerate and display tasks, if any.
+    if tasks.is_empty() {
+        println!("Task list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
+
+    Ok(())
+}
