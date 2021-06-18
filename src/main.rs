5860d2529a867e1126867d34a8ecbd4301c81fb3
Eriberto Mendez <eribert.mendez@gmail.com>
2021-06-18 16:58:52 +0300

    add new fn find_default_journal_file to main

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ mod cli;
 use structopt::StructOpt;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
+// add std path library
+use std::path::PathBuf;
+
+// new function to use the home crate in Cargo.toml
+fn find_default_journal_file() -> Option<PathBuf> {
+	home::home_dir().map(|mut path| {
+		path.push(".rusty-journal.json");
+		path
+	})
+}
+
 
 fn main() {
     // get the command-line args
@@ -15,9 +26,15 @@ fn main() {
 		journal_file,
 	} = CommandLineArgs::from_args();
 
+	// new version
+	let journal_file = journal_file
+	.or_else(find_default_journal_file)
+	.expect("Failed to find journal file.");
+
 	// unpack the journal
-	let journal_file = journal_file.expect("Failed to find journal file");
+	//let journal_file = journal_file.expect("Failed to find journal file");
 	// perform the action with match
+
 	match action {
 		Add { text } => tasks::add_task(journal_file, Task::new(text)),
 		List => tasks::list_tasks(journal_file),
